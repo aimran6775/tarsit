10f18ff763ecaee4e64340fdb746363eca79e4f4
Abdullah <aimran@usf.edu>
2026-01-29 18:53:47 -0700

    fix: update email template exports

--- apps/api/src/mail/templates/index.ts
+++ apps/api/src/mail/templates/index.ts
@@ -4,17 +4,18 @@ export * from './base.template';
 // Auth email templates
 export * from './magic-link.template';
 export * from './password-reset.template';
-export * from './welcome.template';
 export * from './verification.template';
+export * from './welcome.template';
 
 // Appointment email templates
-export * from './appointment-confirmation.template';
-export * from './appointment-request.template';
 export * from './appointment-cancellation.template';
+export * from './appointment-confirmation.template';
 export * from './appointment-reminder.template';
+export * from './appointment-request.template';
 export * from './appointment-status.template';
 
 // Business email templates
 export * from './review-notification.template';
-export * from './verification-status.template';
 export * from './team-invitation.template';
+export * from './verification-status.template';
+
